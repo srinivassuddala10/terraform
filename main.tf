074ada476da89e97bffb58105567e6d4b825ec2d
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-10-01 19:43:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-bucket-12345"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "my_ec2" {
   ami           = "ami-08c40ec9ead489470" # Amazon Linux 2 AMI (check region)
   instance_type = "t2.micro"
 
