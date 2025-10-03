f6c78f6e7a5a042beed354a03ca0366c4e68addb
srinivassuddala10 <srinivassuddala10@gmail.com>
2025-10-03 17:05:12 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,15 @@
-output "s3_bucket" {
-  value = aws_s3_bucket.my_bucket.bucket
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.postgres.endpoint
+output "db_host" {
+  value = aws_db_instance.postgres.address
 }
 
 output "ecr_repo_url" {
-  value = aws_ecr_repository.my_repo.repository_url
+  value = aws_ecr_repository.backend.repository_url
+}
+
+output "frontend_bucket" {
+  value = aws_s3_bucket.frontend.bucket
+}
+
+output "backend_ec2_ip" {
+  value = aws_instance.backend.public_ip
 }
